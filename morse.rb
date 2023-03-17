45243aa27f9cb4671de1a2f00b7f0e50c41e7256
Thinus <thinusvdv2001@gmail.com>
2023-03-17 15:48:28 +0200

    Fix linters

--- morse.rb
+++ morse.rb
@@ -6,8 +6,7 @@ def decode_char(char)
     'V' => '...-', 'W' => '.--', 'X' => '-..-', 'Y' => '-.--', 'Z' => '--..'
   }
 
-  morse_code = keys.key(char)
-  return morse_code
+  keys.key(char)
 end
 
 def decode_word(word)
@@ -16,19 +15,19 @@ def decode_word(word)
   arr_word.each do |ch|
     result += decode_char(ch)
   end
-  return result
+  result
 end
 
 def decode(words)
   array_words = words.split('   ')
   arr = []
   array_words.each do |word|
-    arr.push(decode_word(word))
+    arr << decode_word(word)
   end
-  return arr.join(' ')
+  arr.join(' ')
 end
 
 # Example usage
-input_string = ".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ..."
+input_string = '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
 decoded_string = decode(input_string)
 puts decoded_string # Output: A BOX FULL OF RUBIES
